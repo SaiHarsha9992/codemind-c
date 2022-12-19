1cebfa23406ad18668fc9f0f25b621cc80f6ad2a
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2022-12-19 22:16:24 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);\n    if(y*z>=x)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);\n    if(y*z>=x)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}

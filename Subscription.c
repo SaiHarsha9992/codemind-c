2f8c306422e8f2451bea22a7d2e9268d1a61ece0
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2022-12-28 22:31:01 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int x;         scanf("%d",&x);         if(x>30)         {             printf("YES ");         }         else         {             printf("NO ");         }     } }

---
+++ Subscription.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x>30)
+        {
+            printf("YES
+");
+        }
+        else 
+        {
+            printf("NO
+");
+        }
+    }
+}

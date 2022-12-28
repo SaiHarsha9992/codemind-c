99fd18b73f7b7725de06c8b95e777aa7f8b32b2d
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2022-12-28 22:28:13 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int a,b,c,d;         scanf("%d%d",&a,&b);         c=a*5;         d=b*7;         printf("%d ",c+d);     } }

---
+++ Tour.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,c,d;
+        scanf("%d%d",&a,&b);
+        c=a*5;
+        d=b*7;
+        printf("%d
+",c+d);
+    }
+}

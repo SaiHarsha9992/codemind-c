c5ff46732cfda76dbd01a855e23c91289eef65ad
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-01-06 11:30:29 +0530

    #include <stdio.h> #include <string.h> #include <math.h> #include <stdlib.h> int main() {     char name[100];     scanf("%[^ ]s",name);     printf("Hello Technicalhub ");     printf("%s",name); }

---
+++ Hello_Technicalhub.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    char name[100];
+    scanf("%[^
+]s",name);
+    printf("Hello Technicalhub
+");
+    printf("%s",name);
+}

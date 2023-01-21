5046eb0ae30a98323a92bb7a30616b2524e3d9f1
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-01-21 20:42:59 +0530

    #include <stdio.h> int main ( ) {     char s [ 1000 ] ;     scanf ( "%[^n]s" , s ) ;     int sum = 0 ;     for ( int  i = 0 ; s [ i ] != NULL ; i++ )     {         if ( s [ i ] >=48 && s [ i ] <= 57 )         {             sum = sum + int ( s [ i ] - '0' ) ;         }     }     printf ( "%d" , sum ) ; }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ( )
+{
+    char s [ 1000 ] ;
+    scanf ( "%[^n]s" , s ) ;
+    int sum = 0 ; 
+    for ( int  i = 0 ; s [ i ] != NULL ; i++ )
+    {
+        if ( s [ i ] >=48 && s [ i ] <= 57 )
+        {
+            sum = sum + int ( s [ i ] - '0' ) ;
+        }
+    }
+    printf ( "%d" , sum ) ; 
+}

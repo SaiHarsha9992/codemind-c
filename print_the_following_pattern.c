39d484cf567f35b0b2386386112e7caf8558634f
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-04 07:02:24 +0530

    #include <stdio.h> int main ( ) {     int n ;     scanf ( "%d" , &n ) ;     for (int i = n ; i >= 1 ; i-- )     {         for ( int j = 1 ; j < i ; j++ )         {             printf (" ") ;         }         for ( int k = 1 ; k <= n ; k++ )         {             if ( i == 1 || i == n || k == 1 || k == n )             printf ( "*" ) ;             else             printf ( " " ) ;         }         printf ( " " ) ;     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-int main ()
+int main ( )
 {
-    int x ;
-    scanf ( "%d" , &x ) ;
-    for ( int i = 1 ; i <= x ; i++ )
+    int n ;
+    scanf ( "%d" , &n ) ;
+    for (int i = n ; i >= 1 ; i-- )
     {
-        for ( int j = x ; j > i ; j-- )
+        for ( int j = 1 ; j < i ; j++ )
         {
             printf (" ") ;
         }
-        for ( int k = 1 ; k <= 2 * i - 1 ; k++ )
+        for ( int k = 1 ; k <= n ; k++ )
         {
-            printf ( "%d" , i ) ;
+            if ( i == 1 || i == n || k == 1 || k == n )
+            printf ( "*" ) ;
+            else
+            printf ( " " ) ;
         }
-        printf ("
-") ;
+        printf ( "
+" ) ;
     }
 }

d551ceca6499d6c834e60fc08c72e3c6e0175499
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-06-24 16:07:56 +0530

    #include <stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int n ;         scanf("%d",&n);         int temp = n;         int r = 0 , s = 0 , f = 1;         while(n!=0)         {             r = n%10;             for(int i = 1 ; i <= r ; i++)             {                 f = f *i;             }             s += f;             f = 1;             r = 0;             n = n/10;         }         if(s == temp)         {             printf("Strong ");         }         else         {             printf("Not Strong ");         }     } }

--- Strong_Number.c
+++ Strong_Number.c
@@ -1,28 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,i,f,s=0,r,temp;
-    scanf("%d",&n);
-    temp=n;
-    while(n)
+    int t;
+    scanf("%d",&t);
+    while(t--)
     {
-        int i=1,f=1;
-        r=n%10;
-        while(i<=r)
+        int n ;
+        scanf("%d",&n);
+        int temp = n;
+        int r = 0 , s = 0 , f = 1;
+        while(n!=0)
         {
-            f=f*i;
-            i++;
+            r = n%10;
+            for(int i = 1 ; i <= r ; i++)
+            {
+                f = f *i;
+            }
+            s += f;
+            f = 1;
+            r = 0;
+            n = n/10;
         }
-        s=s+f;
-        n=n/10;
-        
-    }
-    if(s==temp)
-    {
-        printf("The number %d is a strong number",temp);
-    }
-    else
-    {
-        printf("The number %d is not a strong number",temp);
+        if(s == temp)
+        {
+            printf("Strong
+");
+        }
+        else
+        {
+            printf("Not Strong
+");
+        }
     }
 }

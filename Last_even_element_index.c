8da7510a5f57b9b6be44de5d5e196195d9099b61
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-09-21 14:07:48 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin >> n;     vector <int> v;     for(int i = 0; i < n; i++){         int in;         cin >> in;         v.push_back(in);     }     for(int i = n-1; i >= 0 ; i--){         if(v[i]%2==0){             cout << i << endl;             break;         }     } }

---
+++ Last_even_element_index.c
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector <int> v;
+    for(int i = 0; i < n; i++){
+        int in;
+        cin >> in;
+        v.push_back(in);
+    }
+    for(int i = n-1; i >= 0 ; i--){
+        if(v[i]%2==0){
+            cout << i << endl;
+            break;
+        }
+    }
+}
